5dca0311af911ea9530c362b13c0180ccadb3327
Cauê Melo <cauesmelo@gmail.com>
2020-07-23 22:28:39 -0300

    upload de imagem finalizado

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import routes from './routes/index';
 import 'reflect-metadata';
+import uploadConfig from './config/upload';
 
 import './database';
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/', routes);
+app.use('/file', express.static(uploadConfig.directory));
 
 app.listen(3333, () => {
   console.log('Server Running. Port: 3333');
