62f57f7937e3732962fc1e165d2fe8f4c1cc774b
Cauê Melo <cauesmelo@gmail.com>
2020-08-04 19:50:45 -0300

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
+import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes/index';
-import 'reflect-metadata';
 import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
 
 import './database';
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use('/', routes);
