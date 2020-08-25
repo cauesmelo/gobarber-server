0814dd1b46462b974e0a9d1e0aab0d95bad493d8
Cauê Melo <cauesmelo@gmail.com>
2020-08-25 10:57:22 -0300

    provider schedule

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,36 @@
+import { injectable, inject } from 'tsyringe';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import Appointment from '../infra/typeorm/entities/Appointment';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+@injectable()
+export default class ListProviderAppointmentsService {
+  constructor(
+    @inject('IAppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    day,
+    year,
+    month,
+  }: IRequest): Promise<Appointment[]> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        day,
+        year,
+        month,
+      },
+    );
+
+    return appointments;
+  }
+}
